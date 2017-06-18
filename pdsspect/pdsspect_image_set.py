f65c745c43c453dc36d9b08b016d97005e4e8cc6
Perry Vargas <perry.vargas@asu.edu>
2017-06-18 14:30:18 -0700

    Change order of properties to be more readable

--- pdsspect/pdsspect_image_set.py
+++ pdsspect/pdsspect_image_set.py
@@ -62,13 +62,12 @@ class PDSSPectImageSet(object):
                 image = ImageStamp(filepath)
                 self.images.append(image)
             except Exception:
-                raise
                 warnings.warn("Unable to open %s" % (filepath))
         self._current_image_index = 0
+        self.current_color_index = 0
+        self._selection_index = 0
         self._zoom = 1.0
         self._center = None
-        self._selection_index = 0
-        self.current_color_index = 0
         self.rois = {color: np.array([]) for color in self.colors}
         self._delta_x = 0
         self._delta_y = 0
@@ -78,9 +77,9 @@ class PDSSPectImageSet(object):
         self._flip_x = False
         self._flip_y = False
         self._swap_xy = False
-        self._mask_rois = np.zeros(self.current_image.shape, dtype=np.bool)
-        self._maskrgb = masktorgb(self._mask_rois, self.color, self.alpha)
-        self._roi_data = self._maskrgb.get_data()
+        mask = np.zeros(self.current_image.shape, dtype=np.bool)
+        self._maskrgb = masktorgb(mask, self.color, self.alpha)
+        self._roi_data = self._maskrgb.get_data().astype(float)
         self._maskrgb_obj = Image(0, 0, self._maskrgb)
 
     def register(self, view):
@@ -92,6 +91,157 @@ class PDSSPectImageSet(object):
     @property
     def filenames(self):
         return [os.path.basename(filepath) for filepath in self.filepaths]
+
+    @property
+    def current_image_index(self):
+        return self._current_image_index
+
+    @current_image_index.setter
+    def current_image_index(self, new_index):
+        self._current_image_index = new_index
+        for view in self._views:
+            view.set_image()
+
+    @property
+    def current_image(self):
+        return self.images[self._current_image_index]
+
+    @property
+    def color(self):
+        return self.colors[self.current_color_index]
+
+    @property
+    def selection_index(self):
+        return self._selection_index
+
+    @selection_index.setter
+    def selection_index(self, new_index):
+        while new_index >= len(self.selection_types):
+            new_index -= len(self.selection_types)
+        while new_index < 0:
+            new_index += len(self.selection_types)
+        self._selection_index = new_index
+
+    @property
+    def selection_type(self):
+        return self.selection_types[self.selection_index]
+
+    @property
+    def zoom(self):
+        return self._zoom
+
+    @zoom.setter
+    def zoom(self, new_zoom):
+        if new_zoom <= 0.0:
+            raise ValueError("Zoom must be a positive value")
+        self._last_zoom = self._zoom
+        self._zoom = float(new_zoom)
+        for view in self._views:
+            view.adjust_pan_size()
+
+    @property
+    def x_radius(self):
+        return self.current_image.shape[1] / 2
+
+    @property
+    def y_radius(self):
+        return self.current_image.shape[0] / 2
+
+    @property
+    def pan_width(self):
+        return self.x_radius / self.zoom
+
+    @property
+    def _last_pan_width(self):
+        return self.x_radius / self._last_zoom
+
+    @property
+    def pan_height(self):
+        return self.y_radius / self.zoom
+
+    @property
+    def _last_pan_height(self):
+        return self.y_radius / self._last_zoom
+
+    def reset_center(self):
+        self._center = self.current_image.get_center()
+
+    def _point_is_in_image(self, point):
+        data_x, data_y = point
+        height, width = self.current_image.shape[:2]
+        in_width = data_x >= -0.5 and data_x <= (width + 0.5)
+        in_height = data_y >= -0.5 and data_y <= (height + 0.5)
+        return in_width and in_height
+
+    def _determine_center_x(self, x):
+        width = self.current_image.shape[1]
+        left_of_left_edge = x - self.pan_width < -0.5
+        right_of_right_edge = x + self.pan_width > (width + 0.5)
+        in_width = not left_of_left_edge and not right_of_right_edge
+        if in_width:
+            center_x = x
+        elif left_of_left_edge:
+            self._move_roi = True
+            center_x = self.pan_width
+        elif right_of_right_edge:
+            self._move_rois = True
+            center_x = width - self.pan_width
+        return center_x
+
+    def _determine_center_y(self, y):
+        height = self.current_image.shape[0]
+        below_bottom = y - self.pan_height < -0.5
+        above_top = y + self.pan_height > (height + 0.5)
+        in_height = not below_bottom and not above_top
+        if in_height:
+            center_y = y
+        elif below_bottom:
+            self._move_rois = True
+            center_y = self.pan_height
+        elif above_top:
+            self._move_rois = True
+            center_y = height - self.pan_height
+        return center_y
+
+    @property
+    def center(self):
+        if self._center is None:
+            self.reset_center()
+        return self._center
+
+    @center.setter
+    def center(self, new_center):
+        if self._point_is_in_image(new_center):
+            x, y = new_center
+            center = (
+                self._determine_center_x(x), self._determine_center_y(y),
+            )
+            if self._move_rois:
+                self._delta_x = self.center[0] - center[0]
+                self._delta_y = self.center[1] - center[1]
+            else:
+                self._delta_x = 0
+                self._delta_y = 0
+            self._center = center
+            for view in self._views:
+                view.move_pan()
+
+    @property
+    def alpha255(self):
+        return self._alpha * 255.
+
+    @property
+    def alpha(self):
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, new_alpha):
+        self._alpha = new_alpha
+        for roi in self.rois_iterator:
+            rows, cols = np.column_stack(roi)
+            self._roi_data[rows, cols, 3] = self.alpha255
+        for view in self._views:
+            view.set_roi_data()
 
     @property
     def flip_x(self):
@@ -130,169 +280,21 @@ class PDSSPectImageSet(object):
     @property
     def rois_iterator(self):
         rois = [roi for roi in self.rois.values() if roi.size > 0]
-        return iter(rois)
+        return rois
 
-    @property
-    def alpha(self):
-        return self._alpha
+    # @property
+    # def _image_radius(self):
+    #     height, width = self.current_image.shape[:2]
+    #     image_radius = width / 2 if width < height else height / 2
+    #     return image_radius
 
-    @alpha.setter
-    def alpha(self, new_alpha):
-        self._alpha = new_alpha
-        alpah255 = new_alpha * 255.
-        for roi in self.rois_iterator:
-            rows, cols = np.column_stack(roi)
-            self._roi_data[rows, cols, 3] = alpah255
-        for view in self._views:
-            view.set_roi_data()
+    # @property
+    # def radius(self):
+    #     return self._image_radius / self.zoom
 
-    @property
-    def color(self):
-        return self.colors[self.current_color_index]
-
-    @property
-    def selection_index(self):
-        return self._selection_index
-
-    @selection_index.setter
-    def selection_index(self, new_index):
-        while new_index >= len(self.selection_types):
-            new_index -= len(self.selection_types)
-        while new_index < 0:
-            new_index += len(self.selection_types)
-        self._selection_index = new_index
-
-    @property
-    def selection_type(self):
-        return self.selection_types[self.selection_index]
-
-    @property
-    def zoom(self):
-        return self._zoom
-
-    @zoom.setter
-    def zoom(self, new_zoom):
-        if new_zoom <= 0.0:
-            raise ValueError("Zoom must be a positive value")
-        self._last_zoom = self._zoom
-        self._zoom = new_zoom
-        for view in self._views:
-            view.adjust_pan_size()
-
-    @property
-    def _image_radius(self):
-        height, width = self.current_image.shape[:2]
-        image_radius = width / 2 if width < height else height / 2
-        return image_radius
-
-    @property
-    def x_radius(self):
-        return self.current_image.shape[1] / 2
-
-    @property
-    def y_radius(self):
-        return self.current_image.shape[0] / 2
-
-    @property
-    def radius(self):
-        return self._image_radius / self.zoom
-
-    @property
-    def _last_radius(self):
-        return self._image_radius / self._last_zoom
-
-    @property
-    def pan_width(self):
-        return self.x_radius / self.zoom
-
-    @property
-    def _last_pan_width(self):
-        return self.x_radius / self._last_zoom
-
-    @property
-    def pan_height(self):
-        return self.y_radius / self.zoom
-
-    @property
-    def _last_pan_height(self):
-        return self.y_radius / self._last_zoom
-
-    def _point_is_in_image(self, point):
-        data_x, data_y = point
-        height, width = self.current_image.shape[:2]
-        in_width = data_x >= -0.5 and data_x <= (width + 0.5)
-        in_height = data_y >= -0.5 and data_y <= (height + 0.5)
-        return in_width and in_height
-
-    def _determine_center_x(self, x):
-        width = self.current_image.shape[1]
-        right_of_left_edge = x - self.pan_width >= -0.5
-        left_of_right_edge = x + self.pan_width <= (width + 0.5)
-        in_width = left_of_right_edge and right_of_left_edge
-        if in_width:
-            center_x = x
-        elif not right_of_left_edge:
-            self._move_roi = True
-            center_x = self.pan_width
-        elif not left_of_right_edge:
-            self._move_rois = True
-            center_x = width - self.pan_width
-        return center_x
-
-    def _determine_center_y(self, y):
-        height = self.current_image.shape[0]
-        above_bottom = y - self.pan_height >= -0.5
-        below_top = y + self.pan_height <= (height + 0.5)
-        in_height = above_bottom and below_top
-        if in_height:
-            center_y = y
-        elif not above_bottom:
-            self._move_rois = True
-            center_y = self.pan_height
-        elif not below_top:
-            self._move_rois = True
-            center_y = height - self.pan_height
-        return center_y
-
-    def reset_center(self):
-        self._center = self.current_image.get_center()
-
-    @property
-    def center(self):
-        if self._center is None:
-            self.reset_center()
-        return self._center
-
-    @center.setter
-    def center(self, new_center):
-        if self._point_is_in_image(new_center):
-            x, y = new_center
-            center = (
-                self._determine_center_x(x), self._determine_center_y(y),
-            )
-            if self._move_rois:
-                self._delta_x = self._center[0] - center[0]
-                self._delta_y = self._center[1] - center[1]
-            else:
-                self._delta_x = 0
-                self._delta_y = 0
-            self._center = center
-            for view in self._views:
-                view.move_pan()
-
-    @property
-    def current_image_index(self):
-        return self._current_image_index
-
-    @current_image_index.setter
-    def current_image_index(self, new_index):
-        self._current_image_index = new_index
-        for view in self._views:
-            view.set_image()
-
-    @property
-    def current_image(self):
-        return self.images[self._current_image_index]
+    # @property
+    # def _last_radius(self):
+    #     return self._image_radius / self._last_zoom
 
     @property
     def edges(self):
@@ -304,47 +306,67 @@ class PDSSPectImageSet(object):
         return left, bottom, right, top
 
     @property
-    def pan_data(self):
+    def pan_slice(self):
         x1, y1, x2, y2 = self.edges
-        mask = np.s_[y1:y2:1, x1:x2:1]
-        return self.current_image.get_data()[mask]
+        pan_slice = np.s_[y1:y2:1, x1:x2:1]
+        return pan_slice
 
-    def add_coords_to_roi_data_with_color(self, coords, color):
-        rows, cols = np.column_stack(coords)
-        r, g, b = np.array(ginga_colors.lookup_color(color)) * 255.
-        a = self.alpha * 255.
-        rgba = [r, g, b, a]
-        self._roi_data[rows, cols] = rgba
-        for view in self._views:
-            view.set_roi_data()
-        for roi_name in self.rois:
-            roi_coords = self.rois[roi_name].tolist()
-            for coord in coords.tolist():
-                if coord not in roi_coords and roi_name == color:
-                    roi_coords.append(coord)
-                if coord in roi_coords and roi_name != color:
-                    roi_coords.remove(coord)
-            self.rois[roi_name] = np.array(roi_coords)
+    @property
+    def pan_data(self):
+        return self.current_image.get_data()[self.pan_slice]
 
     @property
     def pan_mask(self):
-        x1, y1, x2, y2 = self.edges
-        mask = np.s_[y1:y2:1, x1:x2:1]
-        return self._roi_data[mask]
+        return self._roi_data[self.pan_slice]
 
-    def _get_canvas_coordinate(self, side, coordinate):
-        if self._last_zoom > self._zoom:
-            canvas_coordinate = side + coordinate
-        elif self._last_zoom < self._zoom:
-            canvas_coordinate = side - coordinate
-        else:
-            canvas_coordinate = coordinate
-        return canvas_coordinate
+    def _get_rgb255_from_color(self, color):
+        r, g, b = np.array(ginga_colors.lookup_color(color)) * 255.
+        return r, g, b
 
-    def map_zoom_point_to_view(self):
-        delta_x = self.pan_width - self._last_pan_width
-        delta_y = self.pan_height - self._last_pan_height
-        return delta_x, delta_y
+    def _get_rgba_from_color(self, color):
+        r, g, b = self._get_rgb255_from_color(color)
+        a = self.alpha * 255.
+        rgba = [r, g, b, a]
+        return rgba
+
+    def _set_coords_in_rois_with_color(self, coords, color):
+        for roi_name in self.rois:
+            roi_coords = self.rois[roi_name].tolist()
+            for coord in coords.tolist():
+                add_coord_to_list = all(
+                    (coord not in roi_coords, roi_name == color)
+                )
+                if add_coord_to_list:
+                    roi_coords.append(coord)
+
+                remove_coord_from_list = all(
+                    (coord in roi_coords, roi_name != color)
+                )
+                if remove_coord_from_list:
+                    roi_coords.remove(coord)
+            self.rois[roi_name] = np.array(roi_coords)
+
+    def add_coords_to_roi_data_with_color(self, coords, color):
+        rows, cols = np.column_stack(coords)
+        rgba = self._get_rgba_from_color(color)
+        self._roi_data[rows, cols] = rgba
+        for view in self._views:
+            view.set_roi_data()
+        self._set_coords_in_rois_with_color(coords, color)
+
+    # def _get_canvas_coordinate(self, side, coordinate):
+    #     if self._last_zoom > self._zoom:
+    #         canvas_coordinate = side + coordinate
+    #     elif self._last_zoom < self._zoom:
+    #         canvas_coordinate = side - coordinate
+    #     else:
+    #         canvas_coordinate = coordinate
+    #     return canvas_coordinate
+
+    # def map_zoom_point_to_view(self):
+    #     delta_x = self.pan_width - self._last_pan_width
+    #     delta_y = self.pan_height - self._last_pan_height
+    #     return delta_x, delta_y
 
     def map_zoom_to_full_view(self):
         center_x1, center_y1 = self.current_image.get_center()
@@ -375,9 +397,6 @@ class PDSSpectImageSetViewBase(object):
         pass
 
     def redraw(self):
-        pass
-
-    def delete_rois_with_color(self, color):
         pass
 
     def set_transforms(self):
